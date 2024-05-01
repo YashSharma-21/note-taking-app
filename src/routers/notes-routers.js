677be02a6abf5dadced9c22c8657b762801bce48
YashSharma-21 <meteor131720@gmail.com>
2024-05-02 00:00:03 +0530

    Added a feature to create notes

--- src/routers/notes-routers.js
+++ src/routers/notes-routers.js
@@ -2,5 +2,41 @@ const express = require("express");
 const {Router} = express;
 const noteRouter = new Router();
 const clientPromise = require("../db-connectivity/mongo-client.js");
+const authenticate = require("../authentication/auth.js");
+const mongodb = require("mongodb");
+
+// This request hanlder function allows a user to add a note
+noteRouter.post("/note-create", authenticate, async (req,res,next) => 
+{
+    
+    const mongoClient = await clientPromise;
+    const note = {};
+    const requestBody = req.body;
+    note.creator = req.user._id;
+    note.heading = requestBody.note.heading;
+    note.body = requestBody.note.body;
+
+    try
+    {
+        if(note.heading.length === 0)
+            return res.status(400).send({ error: "Note heading should contain at least 1 character" });
+
+        else if(note.body.length === 0)
+            return res.status(400).send({ error: "Note body should contain at least 1 character"  });
+
+        const noteResponse = await mongoClient.db("note").collection("notes").insertOne(note);
+        note._id = noteResponse.insertedId;
+
+        res.status(201).send(note);
+    }
+    catch(error)
+    {
+        res.status(500).send();
+    }
+
+}, async (error,req,res,next) => 
+{
+    res.status(500).send();
+});
 
 module.exports = noteRouter;
